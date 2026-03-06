68b4aed097e8bbc71ce1361ff213ad51d43ee7db
Divyam Gupta <divyam7091@gmail.com>
2026-03-07 00:05:50 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import DiscoveryMapWrapper from "@/components/maps";
+import { PropertyListing } from "@/data/property-listing";
+//TODO : Add meta data for this page
+// Page should serve via SSR
+// Do not add "use client" declarative
+
+// TODO: Create a List view for these properties.
+// Use your own imagination while designing, please don't copy Propsoch's current UI.
+// We don't like it either.
+// Add pagination
+// You can modify the Property Listing however you want. If you feel like creating an API and implementing pagination via that, totally your call.
+
+export default async function Page() {
+  return (
+    <div className="w-screen h-screen">
+      <DiscoveryMapWrapper allFilteredData={PropertyListing} />
+    </div>
+  );
+}
